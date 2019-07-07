35ffadf1d4c1a2437f2643d748ec1dceefba7dbc
immayurpanchal <mayurpanchal002@gmail.com>
2019-07-07 22:38:54 +0530

    Added web font support using JS

---
+++ contentScript.js
@@ -0,0 +1,8 @@
+console.log('The color is green.');
+var link = document.createElement('link');
+link.setAttribute(
+  'href',
+  'https://fonts.googleapis.com/css?family=Bree+Serif|Open+Sans&display=swap'
+);
+link.setAttribute('rel', 'stylesheet');
+document.getElementsByTagName('head')[0].appendChild(link);
