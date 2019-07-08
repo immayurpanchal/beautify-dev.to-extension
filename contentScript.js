22f7aac3d694fb76baa33df0389554c63cef8128
Mayur Panchal <mayurpanchal002@gmail.com>
2019-07-08 15:34:04 +0530

    Added article user design and bug fixes

--- contentScript.js
+++ contentScript.js
@@ -1,8 +1,8 @@
 console.log('The color is green.');
 var link = document.createElement('link');
 link.setAttribute(
-  'href',
-  'https://fonts.googleapis.com/css?family=Bree+Serif|Open+Sans|Literata&display=swap'
+    'href',
+    'https://fonts.googleapis.com/css?family=Bree+Serif|Open+Sans|Literata|Noto+Sans+JP|Lato|Nanum+Gothic&display=swap'
 );
 link.setAttribute('rel', 'stylesheet');
 document.getElementsByTagName('head')[0].appendChild(link);
